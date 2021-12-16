f3de5bcfe8fbb04c9fc285dfd171998cdadc23a9
Максим Капустин <maximkapus7in@yandex.ru>
2021-12-16 21:53:34 +0300

    Update CreatingNewSheet.java

--- src/cs/vsu/ru/kapustin/CreatingNewSheet.java
+++ src/cs/vsu/ru/kapustin/CreatingNewSheet.java
@@ -65,14 +65,14 @@ public class CreatingNewSheet {
     }
 
     private void sortWithGivenRepetitions(List<Integer> list) {
-        int temp;
+        int tmp;
 
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
                 if (list.get(j) < list.get(i)) {
-                    temp = list.get(i);
+                    tmp = list.get(i);
                     list.set(i, list.get(j));
-                    list.set(j, temp);
+                    list.set(j, tmp);
                 }
             }
         }
